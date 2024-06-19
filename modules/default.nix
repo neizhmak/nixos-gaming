e6da13d81a8a6a69a6344243670f70a47e8792be
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-06-19 21:33:10 +0300

    Defining the systemTweks module

--- modules/default.nix
+++ modules/default.nix
@@ -6,24 +6,10 @@
   flake.nixosModules = let
     inherit (inputs.nixpkgs.lib) filter attrNames;
   in {
-    pipewireLowLatency = import ./pipewireLowLatency.nix;
 
-    steamCompat = self.lib.mkDeprecated "warn" {} {
-      name = "steamCompat";
-      target = "module";
-      instructions = ''
-        `programs.steam.extraCompatPackages` is now available without
-        the need of the steamCompat module. Please use the option
-        provided by the nixpkgs module instead. See the relevant
-        pull request for more details:
+    systemTweaks = import ./systemTweaks.nix;
 
-        <https://github.com/NixOS/nixpkgs/pull/293564>
-
-        You may remove the steamCompat module to supress this message.
-      '';
-    };
-
-    platformOptimizations = import ./platformOptimizations.nix;
+#    gnomeTweaks = import ./gnomeTweaks.nix;
 
     default = throw ''
       The usage of default module is deprecated as multiple modules are provided by nix-gaming. Please use
